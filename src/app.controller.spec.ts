7f2a8105c6be68fac681dd79eddba28e32e4534c
tanghao <974958672@qq.com>
2023-12-27 10:04:56 +0800

    增加nodemon监听启动

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,3 +1,12 @@
+/*
+ * @Author: tanghao 974958672@qq.com
+ * @Date: 2023-12-27 09:41:54
+ * @LastEditors: tanghao 974958672@qq.com
+ * @LastEditTime: 2023-12-27 10:02:46
+ * @FilePath: \real-world\src\app.controller.spec.ts
+ * @Description:
+ *
+ */
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,3 +1,12 @@
+/*
+ * @Author: tanghao 974958672@qq.com
+ * @Date: 2023-12-27 09:41:54
+ * @LastEditors: tanghao 974958672@qq.com
+ * @LastEditTime: 2023-12-27 10:03:01
+ * @FilePath: \real-world\src\app.service.ts
+ * @Description:
+ *
+ */
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
